a52d6629c4d77978e5bfd99702bf3872cd7344bb
PaoM21 <pao_951113@hotmail.com>
2024-10-29 17:56:45 -0500

    Creación del proyecto.

---
+++ BookingProject.API/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookingProject API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookingProject API v1");
+    c.RoutePrefix = string.Empty;
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BookingProject.Domain/Repositories/IReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookingProject.Domain.Entities;
+
+namespace BookingProject.Domain.Repositories
+{
+    public interface IReservationRepository
+    {
+        Task AddAsync(Reservation reservation);
+        Task<Reservation> GetByIdAsync(int id);
+        Task UpdateAsync(Reservation reservation);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Reservation>> GetAllAsync(DateTime? date = null, int? clientId = null, int? serviceId = null);
+    }
+}
