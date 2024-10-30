3c43e72dc8fe1b0ce54f834ff38b9196a89b9f43
PaoM21 <pao_951113@hotmail.com>
2024-10-30 01:41:57 -0500

    Run dockerfile successfully.

--- BookingProject.API/Program.cs
+++ BookingProject.API/Program.cs
@@ -6,10 +6,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar servicios al contenedor.
 builder.Services.AddControllers();
 
-// Configurar la conexión a la base de datos.
 builder.Services.AddDbContext<BookingContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -21,14 +19,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
