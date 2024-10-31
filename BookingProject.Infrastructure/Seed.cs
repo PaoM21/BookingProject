c380df226c97f4369e977e7d80b2555a42aff6df
PaoM21 <pao_951113@hotmail.com>
2024-10-30 23:50:15 -0500

    Add file Seed for Miagrations.

---
+++ BookingProject.Infrastructure/Seed.cs
@@ -0,0 +1,40 @@
+﻿using BookingProject.Domain.Entities;
+using BookingProject.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingProject.Infrastructure
+{
+    public static class Seed
+    {
+        public static void DbInitializer(BookingContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Clients.Any())
+            {
+                var clients = new List<Client>
+                {
+                    new Client { Name = "Cliente1", Email  = "cliente3@gmail.com" },
+                    new Client { Name = "Cliente2", Email  = "cliente4@gmail.com" },
+                    new Client { Name = "Cliente3", Email  = "cliente5@gmail.com" }
+                };
+
+                context.Clients.AddRange(clients);
+                context.SaveChanges();
+            }
+
+            if (!context.Services.Any())
+            {
+                var services = new List<Service>
+                {
+                    new Service { ServiceName = "Servicio1", Price = 1000 },
+                    new Service { ServiceName = "Servicio2", Price = 300 },
+                    new Service { ServiceName = "Servicio3", Price = 450 }
+                };
+
+                context.Services.AddRange(services);
+                context.SaveChanges();
+            }
+        }
+    }
+}
